0f6f134bbc1d7cd6c6597823b46b7441da6439e5
You Jia <jiayou50@gmail.com>
2017-07-25 18:42:14 -0700

    sleep 2

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -5,24 +5,28 @@ set -e
 echo
 echo "Starting"
 echo
+sleep 2
 sudo apt-get update
 sudo apt-get -y upgrade
 
 echo
 echo "Installing Python packages"
 echo
+sleep 2
 sudo apt-get install -y python3-pip
 sudo pip3 install -U -r requirements.txt
 
 echo
 echo "Configuring notebook"
 echo
+sleep 2
 jupyter notebook --generate-config
 echo "c.NotebookApp.password = u'sha1:72031bc85db3:5cc8788668627116eb35e779128ceeb45f4c055a'" >> ~/.jupyter/jupyter_notebook_config.py
 
 echo
 echo "Setting up runit notebook service"
 echo
+sleep 2
 sudo apt-get install -y runit
 sudo mkdir /etc/service
 sudo ln -s /home/ubuntu/kaggle-ec2-setup/runit/notebook /etc/service/notebook
@@ -30,6 +34,7 @@ sudo ln -s /home/ubuntu/kaggle-ec2-setup/runit/notebook /etc/service/notebook
 echo
 echo "Setting up data volume"
 echo
+sleep 2
 sudo mkdir /data
 if [ ! -e /etc/fstab.orig ]
 then
