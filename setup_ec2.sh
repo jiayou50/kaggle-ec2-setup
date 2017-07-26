bffc5241ffac8ab46abafc750c59a0a15a4bce48
You Jia <jiayou50@gmail.com>
2017-07-25 17:53:10 -0700

    add comments

--- setup_ec2.sh
+++ setup_ec2.sh
@@ -1,16 +1,25 @@
 #!/bin/bash
 
+echo
+echo "Starting"
+echo
 sudo apt-get update
 sudo apt-get -y upgrade
 
-# Python packages
+echo
+echo "Installing Python packages"
+echo
 sudo pip3 install -U -r requirements.txt
 
-# Configure notebook
+echo
+echo "Configuring notebook"
+echo
 jupyter notebook --generate-config
 echo "c.NotebookApp.password = u'sha1:72031bc85db3:5cc8788668627116eb35e779128ceeb45f4c055a'" >> ~/.jupyter/jupyter_notebook_config.py
 
-# Keep notebook running
+echo
+echo "Setting up runit notebook service"
+echo
 sudo apt-get install -y runit
 sudo mkdir /etc/service
 sudo ln -s ./runit/notebook /etc/service/notebook
