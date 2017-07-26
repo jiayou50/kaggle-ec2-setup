51199d855c925b26b111f52ddd2e4e05506e92c8
You Jia <jiayou50@gmail.com>
2017-07-25 18:15:16 -0700

    data volume mount

--- runit/notebook/run.sh
+++ runit/notebook/run.sh
@@ -4,10 +4,5 @@ set -e
 
 exec 2>&1
 
-# Data volume
-mkdir /data
-mount /dev/xvdf /data
-chmod a+w /data
-
 # Notebook
 exec chpst -u ubuntu jupyter notebook --no-browser --port=8888 --ip=* --notebook-dir=/data/
--- setup_ec2.sh
+++ setup_ec2.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 echo
 echo "Starting"
 echo
@@ -23,4 +25,19 @@ echo "Setting up runit notebook service"
 echo
 sudo apt-get install -y runit
 sudo mkdir /etc/service
-sudo ln -s ./runit/notebook /etc/service/notebook
+sudo ln -s /home/ubuntu/kaggle-ec2-setup/runit/notebook /etc/service/notebook
+
+echo
+echo "Setting up data volume"
+echo
+sudo mkdir /data
+# sudo mkfs -t ext4 /dev/xvdf
+# mount /dev/xvdf /data
+
+if [ ! -e /etc/fstab.orig ]
+then
+  sudo cp /etc/fstab /etc/fstab.orig
+fi
+sudo echo "/dev/xvdf       /data   ext4    defaults,nofail        0       2" >> /etc/fstab
+sudo mount -a
+sudo chmod a+w /data
